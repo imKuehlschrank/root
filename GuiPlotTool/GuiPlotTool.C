039c24b70945a44b9d230c8c8c90a8e3af825b6a
Filip Ilic <filip.ilic@cern.ch>
2017-05-10 17:33:39 +0200

    layout fix, control options wip

--- GuiPlotTool/GuiPlotTool.C
+++ GuiPlotTool/GuiPlotTool.C
@@ -41,25 +41,36 @@ public:
 
     void PreviewSelection();
     void Superimpose();
-
     void ClearSelectionListbox();
-    void Toggle_path_display();
 
     string LoadFileFromDialog();
 
+    void ToggleEnableRenameTextbox();
+    void UpdateDistplayListboxes();
+
 private:
+    // GUI elements
     TGMainFrame* fMain;
+    TGLabel*     currdirLabel;
     TGTextEntry* searchBox;
+    TGTextEntry* renameTextbox;
+
     TGListBox*   mainListBox;
     TGListBox*   selectionListBox;
 
-    TFile*      file;
-    TDirectory* baseDir;
+    TGCheckButton* displayPathCheckBox;
+    TGCheckButton* renameCheckbox;
+    TGCheckButton* statsCheckBox;
+    TGCheckButton* tdrCheckBox;
 
-    TGLabel* currdirLabel;
 
     TCanvas* previewCanvas;
     TCanvas* resultCanvas;
+
+    // File Stuff
+    TFile*      file;
+    TDirectory* baseDir;
+
 
     map<Int_t, HistogramInfo> table;
     map<Int_t, HistogramInfo> selection;
@@ -69,7 +80,6 @@ private:
     Int_t freeId = 0;
     Int_t GetNextFreeId() { return freeId++; }
 
-    bool display_full_path = false;
     void InitAll();
 };
 
@@ -86,35 +96,65 @@ MyMainFrame::MyMainFrame(const TGWindow *p, UInt_t w, UInt_t h) {
     TGGroupFrame* quicksearchFrame = new TGGroupFrame(fMain,"Quicksearch");
 
     searchBox = new TGTextEntry(quicksearchFrame);
-    TGCheckButton *displayPathCheckBox = new TGCheckButton(quicksearchFrame,"Display path");
+    displayPathCheckBox = new TGCheckButton(quicksearchFrame,"Display path");
 
     searchBox->MoveResize(120,16,300,20);
     displayPathCheckBox->MoveResize(8,40,100,17);
 
-    quicksearchFrame->AddFrame(searchBox, new TGLayoutHints(kLHintsExpandX ,2,2,2,2));
+    quicksearchFrame->AddFrame(searchBox,           new TGLayoutHints(kLHintsExpandX ,2,2,2,2));
     quicksearchFrame->AddFrame(displayPathCheckBox, new TGLayoutHints(kLHintsLeft | kLHintsTop,2,2,2,2));
 
     // ---- Listboxes
     mainListBox = new TGListBox(fMain, -1, kSunkenFrame);
     selectionListBox = new TGListBox(fMain, -1, kSunkenFrame);
 
-    // ---- Control Frame
+    // --- All Controls
     TGHorizontalFrame* controlFrame = new TGHorizontalFrame(fMain, 200, 40);
 
-    TGTextButton* previewSelectionButton = new TGTextButton(controlFrame, "&Preview Selection List");
-    TGTextButton* superimposeButton = new TGTextButton(controlFrame, "&Superimpose Selection");
-    TGTextButton* clearSelectionButton = new TGTextButton(controlFrame, "&Clear Selection");
+        // ------- Buttons
+    TGVerticalFrame* controlFrameButtons = new TGVerticalFrame(controlFrame, 200, 40);
 
-    controlFrame->AddFrame(previewSelectionButton, new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
-    controlFrame->AddFrame(superimposeButton, new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
-    controlFrame->AddFrame(clearSelectionButton, new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
+    TGTextButton* previewSelectionButton = new TGTextButton(controlFrameButtons, "&Preview List");
+    TGTextButton* superimposeButton      = new TGTextButton(controlFrameButtons, "&Superimpose");
+    TGTextButton* clearSelectionButton   = new TGTextButton(controlFrameButtons, "&Clear Selection");
+
+    controlFrameButtons->AddFrame(clearSelectionButton,   new TGLayoutHints(kLHintsLeft | kLHintsExpandX, 5, 5, 3, 4));
+    controlFrameButtons->AddFrame(previewSelectionButton, new TGLayoutHints(kLHintsLeft | kLHintsExpandX, 5, 5, 3, 4));
+    controlFrameButtons->AddFrame(superimposeButton,      new TGLayoutHints(kLHintsLeft | kLHintsExpandX, 5, 5, 3, 4));
+
+        // ------- Checkboxes
+    TGVerticalFrame* controlFrameCheckboxes = new TGVerticalFrame(controlFrame, 200, 80);
+
+    tdrCheckBox = new TGCheckButton(controlFrameCheckboxes,"Pub. Style");
+    statsCheckBox = new TGCheckButton(controlFrameCheckboxes,"Show Stats");
+    TGCheckButton* cb1 = new TGCheckButton(controlFrameCheckboxes,"Use Colors");
+    TGCheckButton* cb2 = new TGCheckButton(controlFrameCheckboxes,"Add Legend ");
+
+    controlFrameCheckboxes->AddFrame(tdrCheckBox, new TGLayoutHints(kLHintsLeft | kLHintsExpandX, 5, 5, 3, 4));
+    controlFrameCheckboxes->AddFrame(statsCheckBox, new TGLayoutHints(kLHintsLeft | kLHintsExpandX, 5, 5, 3, 4));
+    controlFrameCheckboxes->AddFrame(cb1, new TGLayoutHints(kLHintsLeft | kLHintsExpandX, 5, 5, 3, 4));
+    controlFrameCheckboxes->AddFrame(cb2, new TGLayoutHints(kLHintsLeft | kLHintsExpandX, 5, 5, 3, 4));
+
+        // ------- Rename
+    TGVerticalFrame* controlFrameRename = new TGVerticalFrame(controlFrame, 200, 40);
+
+    renameCheckbox = new TGCheckButton(controlFrameRename,"Use Custom Title");
+    renameTextbox  = new TGTextEntry(controlFrameRename);
+
+    controlFrameRename->AddFrame(renameCheckbox, new TGLayoutHints(kLHintsLeft | kLHintsExpandX, 5, 5, 3, 4));
+    controlFrameRename->AddFrame(renameTextbox,  new TGLayoutHints(kLHintsLeft | kLHintsExpandX, 5, 5, 3, 4));
+
+        // --- Add to All Controls
+    controlFrame->AddFrame(controlFrameButtons,    new TGLayoutHints(kLHintsCenterX, 2, 2, 2, 2));
+    controlFrame->AddFrame(controlFrameCheckboxes, new TGLayoutHints(kLHintsCenterX, 2, 2, 2, 2));
+    controlFrame->AddFrame(controlFrameRename,     new TGLayoutHints(kLHintsCenterX, 2, 2, 2, 2));
 
     // ---- Main Frame, Adding all the individual frames in the appropriate order top to bottom
-    fMain->AddFrame(currdirLabel, new TGLayoutHints(kLHintsCenterX ,2,2,2,2));
-    fMain->AddFrame(quicksearchFrame, new TGLayoutHints(kLHintsLeft | kLHintsTop | kLHintsExpandX,2,2,2,2));
-    fMain->AddFrame(mainListBox, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 5, 5, 5, 6));
-    fMain->AddFrame(selectionListBox, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 5, 5, 6, 7));
-    fMain->AddFrame(controlFrame, new TGLayoutHints(kLHintsCenterX, 2, 2, 2, 2));
+    fMain->AddFrame(currdirLabel,       new TGLayoutHints(kLHintsCenterX ,2,2,2,2));
+    fMain->AddFrame(quicksearchFrame,   new TGLayoutHints(kLHintsLeft | kLHintsTop | kLHintsExpandX,2,2,2,2));
+    fMain->AddFrame(mainListBox,        new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 5, 5, 5, 6));
+    fMain->AddFrame(selectionListBox,   new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 5, 5, 6, 7));
+    fMain->AddFrame(controlFrame,       new TGLayoutHints(kLHintsLeft, 2, 2, 2, 2));
 
     fMain->SetWindowName("Filter & Combine Plots");
     fMain->MapSubwindows();
@@ -122,7 +162,7 @@ MyMainFrame::MyMainFrame(const TGWindow *p, UInt_t w, UInt_t h) {
 
     // #### Signal/Slots ####
     searchBox->Connect("TextChanged(const char *)", "MyMainFrame", this, "FilterBySearchBox()");
-    displayPathCheckBox->Connect("Clicked()", "MyMainFrame", this, "Toggle_path_display()");
+    displayPathCheckBox->Connect("Clicked()", "MyMainFrame", this, "UpdateDistplayListboxes()");
 
     mainListBox->Connect("DoubleClicked(Int_t)", "MyMainFrame", this, "AddToSelection(Int_t)");
     selectionListBox->Connect("DoubleClicked(Int_t)", "MyMainFrame", this, "RemoveFromSelection(Int_t)");
@@ -131,9 +171,11 @@ MyMainFrame::MyMainFrame(const TGWindow *p, UInt_t w, UInt_t h) {
     superimposeButton->Connect("Clicked()", "MyMainFrame", this, "Superimpose()");
     clearSelectionButton->Connect("Clicked()", "MyMainFrame", this, "ClearSelectionListbox()");
 
+    renameCheckbox->Connect("Clicked()", "MyMainFrame", this, "ToggleEnableRenameTextbox()");
+
     // #### Init Window ####
     fMain->MapWindow();
-    fMain->MoveResize(200, 300, 450, 600);
+    fMain->MoveResize(100, 100, 600, 700);
     InitAll();
 }
 
@@ -152,33 +194,41 @@ string MyMainFrame::LoadFileFromDialog()
     return file_info_.fFilename;
 }
 
-void MyMainFrame::InitAll(){
 
-//    string file_name = LoadFileFromDialog();
-    string file_name = "/home/fil/projects/PR/root/GuiPlotTool/DQM_V0001_SiStrip_R000283283.root";
+void MyMainFrame::ToggleEnableRenameTextbox() {
+    renameTextbox->SetEnabled(renameCheckbox->IsDown());
+}
+
+
+void MyMainFrame::InitAll() {
+
+    string file_name = LoadFileFromDialog();
+//    string file_name = "/home/fil/projects/PR/root/GuiPlotTool/DQM_V0001_SiStrip_R000283283.root";
     file = TFile::Open(file_name.c_str());
 
     file ?  cout << "[ OK ]" : cout << "[FAIL]";
     cout << " Opening File" << endl;
 
     string baseDirStr = "DQMData";
-    baseDir = (TDirectory *)file->Get(baseDirStr.c_str());
+    baseDir = (TDirectory*)file->Get(baseDirStr.c_str());
 
     baseDir ?  cout << "[ OK ]" : cout << "[FAIL]";
     cout << " Entering Directory" << endl;
 
     string currentPath = file_name + ":/" + string(baseDirStr);
 
+    // Set up all the GUI elements
     currdirLabel->SetText(currentPath.c_str());
     searchBox->SetText("");
+    renameTextbox->SetEnabled(false);
 
+    // Fill internal data structes from file and display
     LoadAllPlotsFromDir(baseDir);
     DisplayMapInListBox(table, mainListBox);
-    fMain->MoveResize(200, 300, 450, 600);
+    fMain->MoveResize(100, 100, 600, 700);
 }
 
-void MyMainFrame::Toggle_path_display() {
-    display_full_path = !display_full_path;
+void MyMainFrame::UpdateDistplayListboxes() {
     DisplayMapInListBox(table, mainListBox);
     DisplayMapInListBox(selection, selectionListBox);
     FilterBySearchBox();
@@ -186,18 +236,18 @@ void MyMainFrame::Toggle_path_display() {
 
 void MyMainFrame::LoadAllPlotsFromDir(TDirectory *current) {
     TIter next(current->GetListOfKeys());
-    TKey *key;
+    TKey* key;
 
-    while ((key = (TKey *)next())) {
-        TClass *cl = gROOT->GetClass(key->GetClassName());
+    while ((key = (TKey* )next())) {
+        TClass* cl = gROOT->GetClass(key->GetClassName());
 
         if (cl->InheritsFrom("TDirectory")) {
-            TDirectory *d = (TDirectory *)key->ReadObj();
+            TDirectory* d = (TDirectory*)key->ReadObj();
             LoadAllPlotsFromDir(d);
         }
 
         if (cl->InheritsFrom("TH1")) {
-            TH1 *h = (TH1 *)key->ReadObj();
+            TH1* h = (TH1* )key->ReadObj();
             string fname = h->GetName();
             string path = string(current->GetPath()) + "/" + fname;
 
@@ -209,16 +259,14 @@ void MyMainFrame::LoadAllPlotsFromDir(TDirectory *current) {
     }
 }
 
-void MyMainFrame::DisplayMapInListBox(const map<Int_t, HistogramInfo> &m, TGListBox *listbox) {
+void MyMainFrame::DisplayMapInListBox(const map<Int_t, HistogramInfo>& m, TGListBox* listbox) {
     listbox->RemoveAll();
 
-    for (auto &elem : m) {
-        if(display_full_path)
-            listbox->AddEntry(elem.second.GetPath().c_str(), elem.first);
-        else
-            listbox->AddEntry(elem.second.GetName().c_str(), elem.first);
+    string disp;
+    for (auto& elem : m) {
+        displayPathCheckBox->IsDown() ? disp = elem.second.GetPath() : disp = elem.second.GetName();
+        listbox->AddEntry(disp.c_str(), elem.first);
     }
-
     listbox->SortByName();
 }
 
@@ -269,9 +317,26 @@ void MyMainFrame::Superimpose() {
     resultCanvas = new TCanvas("Superimpose Canvas", "", 800, 400);
     resultCanvas->cd();
 
+    bool firstElem = true;
+    string title;
+    TH1* out;
     for(auto& elem : selection){
-        ((TH1*)elem.second.GetObj())->SetStats(false);
-        elem.second.GetObj()->Draw("same");
+
+        if(firstElem){
+            out = (TH1*)elem.second.GetObj()->Clone();
+
+            (renameCheckbox->IsOn()) ? title=renameTextbox->GetText(): title=out->GetTitle();
+            out->SetTitle(title.c_str());
+
+            out->SetStats(statsCheckBox->IsOn());  // FIXME: only shows the first statbox
+            out->Draw("same");
+            firstElem=false;
+
+        } else {
+            ((TH1*)elem.second.GetObj())->SetStats(statsCheckBox->IsOn()); // FIXME: only shows the first statbox
+            elem.second.GetObj()->Draw("same");
+
+        }
     }
 }
 
